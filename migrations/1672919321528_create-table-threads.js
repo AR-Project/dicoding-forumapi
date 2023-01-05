8c14e02f0d8b8d47fc08f1348d25ee2dd6ce6f06
AR Project <alvenresta@gmail.com>
2023-01-05 19:38:34 +0700

    create threads table

---
+++ migrations/1672919321528_create-table-threads.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  })
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
