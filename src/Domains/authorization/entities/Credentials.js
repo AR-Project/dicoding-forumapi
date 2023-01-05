419640688baa0a7683a035bd225a6323137d139e
AR Project <alvenresta@gmail.com>
2023-01-06 01:51:44 +0700

    adding credentials entities

---
+++ src/Domains/authorization/entities/Credentials.js
@@ -0,0 +1,22 @@
+class Credentials {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.id = payload.id;
+    this.username = payload.username;
+  }
+
+  _verifyPayload(payload) {
+    const {id, username} = payload;
+
+    if (!id || !username) {
+      throw new Error('CREDENTIALS.NOT_CONTAIN_NEEDED_PROPERTY')
+    }
+
+    if (typeof id !== 'string' || typeof username !== 'string') {
+      throw new Error ('CREDENTIALS.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    }
+  }
+}
+
+module.exports = Credentials;
---
+++ src/Domains/authorization/entities/_test/Credentials.test.js
@@ -0,0 +1,38 @@
+const Credentials = require('../Credentials')
+
+describe('Credentials entities', () => {
+  it('should throw error when credentials does not contain needed property', () => {
+    // Arrange
+    const credentials = {
+      username: 'username'
+    }
+
+    expect(() => new Credentials(credentials)).toThrowError('CREDENTIALS.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when credentials not meet data type specification', () => {
+    // Arrange
+    const credentials = {
+      id: 123,
+      username: 'username'
+    }
+
+    expect(() => new Credentials(credentials)).toThrowError('CREDENTIALS.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create credentials entities correctly', () => {
+    // Arrange
+    const credentials = {
+      id: 'user-123',
+      username: 'username'
+    };
+
+    // Action
+    const newCredentials = new Credentials(credentials);
+
+    // Assert
+    expect(newCredentials).toBeInstanceOf(Credentials);
+    expect(newCredentials.id).toEqual(credentials.id);
+    expect(newCredentials.username).toEqual(credentials.username);
+  });
+})
