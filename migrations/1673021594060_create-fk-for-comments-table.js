18b105135292e4e2eab09b984eeddba1e10703e9
AR Project <alvenresta@gmail.com>
2023-01-07 00:46:21 +0700

    create table migration for comment

---
+++ migrations/1673021594060_create-fk-for-comments-table.js
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.addConstraint('comments','fk_comments.owner_user.id', 'FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments','fk_comments.thread_id__threads.id', 'FOREIGN KEY (thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('comments', 'fk_comments.thread_id__threads.id');
+  pgm.dropConstraint('comments', 'fk_comments.owner_user.id');
+};
