005a4f03ee1d2eb14dde00a5443b1dd0d3c525de
AR Project <alvenresta@gmail.com>
2023-01-09 22:40:11 +0700

    implement DeleteComment UseCase

---
+++ src/Applications/use_case/DeleteCommentUseCase.js
@@ -0,0 +1,22 @@
+class DeleteCommentUseCase {
+  constructor({ commentRepository, threadRepository }) {
+    this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(threadId, commentId, userId) {
+    this._verifyParameter(threadId, commentId, userId);
+    await this._threadRepository.verifyThread(threadId);
+    await this._commentRepository.verifyComment(commentId);
+    await this._commentRepository.verifyCommentOwner(commentId, userId);
+    await this._commentRepository.deleteComment(commentId);
+  }
+  
+  _verifyParameter(threadId, commentId, userId) {
+    if (!threadId || !commentId || !userId) {
+      throw new Error('DELETE_COMMENT_USECASE.MISSING_PARAM')
+    }
+  }
+}
+
+module.exports = DeleteCommentUseCase;
---
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -0,0 +1,60 @@
+const CommentRepository = require("../../../Domains/comments/CommentRepository");
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository')
+
+const DeleteCommentUseCase = require("../DeleteCommentUseCase");
+
+describe('DeleteCommentUseCase', () => { 
+  it('should throw error if any params is missing', async () => {
+    const threadId = 'thread_id'
+    const commentId = 'comment_id';
+    const userId = 'user_id';
+
+    const mockCommentRepository = new CommentRepository();
+    const mockThreadRepository = new ThreadRepository();
+
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    
+    await expect(deleteCommentUseCase.execute(undefined, commentId, userId)).rejects
+      .toThrowError('DELETE_COMMENT_USECASE.MISSING_PARAM')
+    await expect(deleteCommentUseCase.execute(threadId, undefined, userId)).rejects
+      .toThrowError('DELETE_COMMENT_USECASE.MISSING_PARAM')
+    await expect(deleteCommentUseCase.execute(threadId, commentId, undefined)).rejects
+      .toThrowError('DELETE_COMMENT_USECASE.MISSING_PARAM')
+  })
+
+
+  it('should orchestrating the deleting comment action correctly', async () => { 
+    // Arrange
+    const threadId = 'thread_id'
+    const commentId = 'comment_id';
+    const userId = 'user_id';
+
+    const mockCommentRepository = new CommentRepository();
+    mockCommentRepository.verifyComment = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyCommentOwner = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.deleteComment = jest.fn()
+    .mockImplementation(() => Promise.resolve());
+
+    const mockThreadRepository = new ThreadRepository();
+    mockThreadRepository.verifyThread = jest.fn()
+      .mockImplementation(() => Promise.resolve())
+
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+        commentRepository: mockCommentRepository,
+        threadRepository: mockThreadRepository,
+    });
+
+    await deleteCommentUseCase.execute(threadId, commentId, userId);
+
+    expect(mockThreadRepository.verifyThread).toBeCalledWith(threadId);
+    expect(mockCommentRepository.verifyComment).toBeCalledWith(commentId);
+    expect(mockCommentRepository.verifyCommentOwner).toBeCalledWith(commentId, userId);
+    expect(mockCommentRepository.deleteComment).toBeCalledWith(commentId);
+   })  
+})
